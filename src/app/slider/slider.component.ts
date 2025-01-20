3d8ecacf7aac41ac300af367018206a380eccee8
Philip Eze <philchukwueze@outlook.com>
2025-01-21 00:47:56 +0100

    additionn of slider logic

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { BaseIconComponent } from '../base-icon/base-icon.component';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -7,4 +6,40 @@ import { BaseIconComponent } from '../base-icon/base-icon.component';
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
-export class SliderComponent {}
+export class SliderComponent implements OnInit, OnDestroy {
+  // Array of images for the slider
+  public images: string[] = [
+    'bilder-und-icons/sw.png', // Example images (update these paths)
+    'bilder-und-icons/sw@2x.png',
+    'bilder-und-icons/sw@3x.png',
+  ];
+
+  public currentImageIndex: number = 0;
+
+  private imageChangeInterval: any;
+
+  public ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  public ngOnDestroy() {
+    if (this.imageChangeInterval) {
+      clearInterval(this.imageChangeInterval);
+    }
+  }
+
+  public startAutoSlide() {
+    this.imageChangeInterval = setInterval(() => {
+      this.nextImage();
+    }, 5000);
+  }
+
+  public nextImage() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+  }
+
+  public prevImage() {
+    this.currentImageIndex =
+      (this.currentImageIndex - 1 + this.images.length) % this.images.length;
+  }
+}
