fa6211478c70d5c7026658dcaa7be73821bb6c9f
Philip Eze <philchukwueze@outlook.com>
2025-01-20 12:34:56 +0100

    installation of bootstrap

---
+++ src/app/base-icon/base-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-icon',
+  imports: [],
+  templateUrl: './base-icon.component.html',
+  styleUrl: './base-icon.component.scss'
+})
+export class BaseIconComponent {
+
+}
